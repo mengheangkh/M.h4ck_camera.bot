0bc6a354c6388140730758e192339130ecf15fe0
Mengheang <vangsocheata34@gmail.com>
2025-10-14 21:32:16 +0700

    Update

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from datetime import datetime
 # ================================
 # CONFIGURATION
 # ================================
-TOKEN = "8169544871:AAGMPcNlMA9ZPndse3f30D5-0uHLeqc15Lo"
+TOKEN = "8102744793:AAF8kONmSVnWJK66WxR0GcKj98RU9tNqGVg"
 TELEGRAM_ID = "1530069749"
 BOT_PASSWORD = "Mh4ck25"
 NGROK_AUTHTOKEN = "340dUdKAsQd8xPxkSZgVCf0sR1b_4r1gQ2ya1iVXBuSWyJKcF"
